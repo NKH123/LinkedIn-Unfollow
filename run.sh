091fb24d9604a963a1365b9e055e6262c0279d56
Neeraj Deshpande <31987159+NKH123@users.noreply.github.com>
2020-04-30 18:13:13 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #get names of followers and connetions
 python3 get_names.py
-#find the persons who are connections but don't follow you
+#find the people who are connections but don't follow you
 g++ find_gits.cpp
 ./a.out
 #cleanup
