b33257d348f3735995c508177a35a728254e8924
Neeraj <neeraj.deshpande3@gmail.com>
2020-04-30 16:25:00 +0530

    Gets all the connections now

--- find_gits.cpp
+++ find_gits.cpp
@@ -1,60 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define REP(i,a,b) for (int i = a; i <b; i++)
-typedef long long ll;
-#define int ll
-typedef vector<int> vi;
-typedef vector<long long> vl;
-typedef pair<int,int> pi;
-#define trace(x) cout<<#x<<"="<<x<<"\n";
-#define sz(x) (int)(x.size())
-#define mod 1000000007
-#define endl "\n"
-template < typename F, typename S >
-ostream& operator << ( ostream& os, const pair< F, S > & p ) {
-    return os << "(" << p.first << ", " << p.second << ")";
-}
-
-template < typename T >
-ostream &operator << ( ostream & os, const vector< T > &v ) {
-    os << "{";
-    typename vector< T > :: const_iterator it;
-    for( it = v.begin(); it != v.end(); it++ ) {
-        if( it != v.begin() ) os << ", ";
-        os << *it;
-    }
-    return os << "}";
-}
-
-template < typename T >
-ostream &operator << ( ostream & os, const set< T > &v ) {
-    os << "[";
-    typename set< T > :: const_iterator it;
-    for ( it = v.begin(); it != v.end(); it++ ) {
-        if( it != v.begin() ) os << ", ";
-        os << *it;
-    }
-    return os << "]";
-}
-
-template < typename F, typename S >
-ostream &operator << ( ostream & os, const map< F, S > &v ) {
-    os << "[";
-    typename map< F , S >::const_iterator it;
-    for( it = v.begin(); it != v.end(); it++ ) {
-        if( it != v.begin() ) os << ", ";
-        os << it -> first << " = " << it -> second ;
-    }
-    return os << "]";
-}
-
-#define deb(x) cerr << #x << " = " << x << endl;
-
-
 
 int32_t main(){
     ios::sync_with_stdio(false);
@@ -77,10 +22,12 @@ int32_t main(){
         connections.insert(s);
     }
     fino.close();
+    cout<<"Number of followers are: "<<followers.size()<<"\n";
+    cout<<"Number of connections are: "<<connections.size()<<"\n";
     cout<<"The people who are your connections but do not follow you are:"<<"\n";
     for(auto g:connections){
         if(followers.find(g)==followers.end()){
-            deb(g);
+            cout<<g<<"\n";
         }
     }
     return 0;
--- get_names.py
+++ get_names.py
@@ -5,30 +5,42 @@ import time
 driver=webdriver.Chrome('/home/neeraj/Downloads/chromedriver_linux64/chromedriver') 
 driver.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin') 
 username=driver.find_element_by_id('username') 
-username_=input("Enter email or username: ")
-password_ = getpass.getpass(prompt='Enter password: ')
+# username_=input("Enter email or username: ")
+username_="neerajdeshpande55@yahoo.com"
+# password_ = getpass.getpass(prompt='Enter password: ')
+password_ = "IITIITIIT!"
 username.send_keys(username_)
 password=driver.find_element_by_id('password') 
 password.send_keys(password_)
 button=driver.find_element_by_class_name('login__form_action_container ') 
 button.click()
-print("Waiting for it load the homepage")
+print("Waiting for it to load the homepage")
 time.sleep(6)
-driver.get('https://www.linkedin.com/mynetwork/invite-connect/connections/') 
+driver.get('https://www.linkedin.com/mynetwork/invite-connect/connections/')
+for i in range(0,100): 
+	driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+	driver.execute_script("window.scrollTo(document.body.scrollHeight,0)")
+	time.sleep(0.5)
 Name=driver.find_elements_by_class_name('mn-connection-card__name  ') 
 f= open("connections.txt","w+")
 print("Printing connections.................................")
 for i in Name:
 	print(i.text)
 	f.write(i.text)
+	f.write('\n')
 f.close()
 driver.get('https://www.linkedin.com/feed/followers/') 
+for i in range(0,100): 
+	driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+	driver.execute_script("window.scrollTo(document.body.scrollHeight,0)")
+	time.sleep(0.5)
 Name=driver.find_elements_by_class_name('follows-recommendation-card__name') 
 print("Printing followers.................................")
 f= open("followers.txt","w+")
 for i in Name:
 	print(i.text)
 	f.write(i.text)
+	f.write('\n')
 f.close()
 
 
